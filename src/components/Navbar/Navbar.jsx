e6659c06db84ba490345225731c97660313cfe08
Dynosaur <heracassia@gmail.com>
2019-12-26 22:43:23 -0600

    Navbar improvements, added graph link

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-import algebruhLogo from '../../../dist/algebruhLogo.png';
+import logoImage from './logo.png';
 
 import './Navbar.scss';
 
@@ -10,11 +10,16 @@ class Navbar extends Component {
         return (
             <div className='alg-navbar-container'>
                 <Link to='/'>
-                    <img className='alg-navbar-logo-img' src={algebruhLogo} />
+                    <img className='alg-navbar-logo-img' src={logoImage} />
                 </Link>
-                <Link to='/sign-in'>
-                    <h6 className='alg-navbar-sign-in'>Sign In</h6>
-                </Link>
+                <div className='alg-navbar-link-container'>
+                    <Link to='/graph' className='alg-navbar-graph-anchor'>
+                        <h6 className='alg-navbar-text'>Graph</h6>
+                    </Link>
+                    <Link to='/sign-in'>
+                        <h6 className='alg-navbar-text'>Sign In</h6>
+                    </Link>
+                </div>
             </div>
         );
     }
