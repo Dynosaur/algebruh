760c24686eadbe913379d2546153047346ef761c
Dynosaur <heracassia@gmail.com>
2019-12-26 22:29:58 -0600

    Server cleanup and ignored some generated files

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require("express")
 const server = express()
 
-const serverPort = 8080;
+const serverPort = 80;
 
 const htmlFile = __dirname + '/dist/index.html';
-const scriptFile = __dirname + '/dist/bundle.js';
-const iconFile = __dirname + '/dist/favicon.ico';
-const styleFile = __dirname + '/dist/index.css';
 
 server.use(
     express.static(__dirname + '/dist')
