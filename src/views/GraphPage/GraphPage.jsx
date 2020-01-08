64b2462a3298d98030219ebcbd2e547cb21a0908
Dynosaur <heracassia@gmail.com>
2020-01-07 19:53:27 -0600

    Added SCSS

--- src/views/GraphPage/GraphPage.jsx
+++ src/views/GraphPage/GraphPage.jsx
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
-import Container from '../../components/Container';
-import Graph from '../../components/graph';
+
+import GraphToolBar from '../../components/GraphToolBar';
+import Graph        from '../../components/Graph';
+import Page         from '../../components/Page';
+
+import './GraphPage.scss';
 
 class GraphPage extends Component {
 
     render() {
         return (
-            <Graph />
+            <Page idName='alg-graph-view'>
+                <Graph />
+                <GraphToolBar />
+            </Page>
         );
     }
-
 }
 
 export default GraphPage;
