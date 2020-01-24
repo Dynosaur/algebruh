7cec5e5b6027d0d446f072410ecba9ff009d3ab6
Dynosaur <heracassia@gmail.com>
2020-01-24 00:38:31 -0600

    Wiped clean

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from 'Home/Home';
-
-console.log('Index.js');
-
-//ReactDOM.render(<Home />, document.getElementById('root'));
