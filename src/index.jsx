3521ec55104a9e3a88864e47f012319f695bd624
Dynosaur <heracassia@gmail.com>
2020-01-29 00:08:47 -0600

    Prettified

--- src/index.jsx
+++ src/index.jsx
@@ -2,23 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import HomePage     from './views/HomePage';
-import SignInPage   from './views/SignInPage';
-import GraphPage    from './views/GraphPage';
-import BlankPage    from './views/BlankPage';
+import GraphPage    from './views/graphPage';
+import HomePage     from './views/homePage';
 import NotFoundPage from './views/NotFoundPage';
+import SignInPage   from './views/signinPage';
 
 import './global.scss';
 
 const frontEndRouter = (
     <Router>
         <Switch>
-            <Route exact path='/' component={HomePage} />
-            <Route exact path='/home' component={HomePage} />
-            <Route exact path='/sign-in' component={SignInPage} />
-            <Route exact path='/graph' component={GraphPage} />
-            <Route exact path='/blank' component={BlankPage} />
-            <Route component={NotFoundPage} />
+            <Route exact path='/'        component={ HomePage     } />
+            <Route exact path='/home'    component={ HomePage     } />
+            <Route exact path='/sign-in' component={ SignInPage   } />
+            <Route exact path='/graph'   component={ GraphPage    } />
+            <Route                       component={ NotFoundPage } />
         </Switch>
     </Router>
 );
