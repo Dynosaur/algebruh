853dadbbaaf9153f254604728168aad26e1a8f6c
Dynosaur <heracassia@gmail.com>
2020-02-16 23:01:12 -0600

    Shortened component down, removed mathquill

--- src/components/toolbar/toolbar.tsx
+++ src/components/toolbar/toolbar.tsx
@@ -1,48 +1,27 @@
-import * as React from 'react';
-import { addStyles, EditableMathField } from 'react-mathquill';
-
+import React, { FC, KeyboardEvent, useState } from 'react';
+import Button from '../button';
+import MathField from '../mathfield';
 import './toolbar-style';
 
-addStyles();
+interface ToolbarProps {}
 
-const handleKeyPress = (index: number, ev: KeyboardEvent): void => {
-    if (ev.key == 'Enter') {
-        handleEnterKey(index, ev);
-    }
-}
+const Toolbar: FC<ToolbarProps> = (props) => {
+    const [quant, setQuant] = useState(1);
 
-const handleEnterKey = (index: number, ev: KeyboardEvent): void => {
-    const next = document.getElementById('alg-math-field-' + (index + 1));
-    if (!next) {
-        console.log('add new one');
-    } else {
-        next.focus();
-    }
-}
-
-type ToolbarProps = {
-    width: number;
-    height: number;
-    fieldNum: number;
-    onChange?: (latex: string) => void;
-    onKeyPress?: (index: number, ev: KeyboardEvent) => void;
-}
-
-const Toolbar = (props: ToolbarProps): JSX.Element => {
     let mathFields: JSX.Element[] = [];
-    for (let i = 0; i < props.fieldNum; i++) {
+    for (let i = 0; i < quant - 1; i++) {
         mathFields.push(
-            <EditableMathField
-                key={i.toString()}
-                id={'alg-math-field-' + i}
-                className='alg-math-field'
-                onKeyPress={ (ev: KeyboardEvent) => handleKeyPress(i, ev)}
-            />
+            <MathField key={i} />
         );
     }
+    mathFields.push(
+        <MathField key={quant - 1} />
+    );
+
     return(
         <div className='alg-toolbar'>
             {mathFields}
+            <Button onClick={() => setQuant(quant + 1)} />
         </div>
     );
 }
