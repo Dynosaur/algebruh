7763c9d4eb5dc4e8f0ba58df13a9d2f3d09e8e48
Dynosaur <heracassia@gmail.com>
2020-02-16 23:03:03 -0600

    Renamed stylesheet

--- src/views/notFoundPage/notFoundPage.jsx
+++ src/views/notFoundPage/notFoundPage.jsx
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
 
-import './NotFoundPage';
+import Page from '../../components/page';
+
+import './notFoundPage-style';
 
 class NotFoundPage extends Component {
 
     render() {
         return(
-            <div className='alg-not-found-view'>
-                <h1>Sorry!</h1>
-                <h2>The page you were looking for could not be found!</h2>
-            </div>
+            <Page navbar>
+                <div className='alg-not-found-view'>
+                    <h1>Sorry!</h1>
+                    <h2>The page you were looking for could not be found!</h2>
+                </div>
+            </Page>
         );
     }
 
