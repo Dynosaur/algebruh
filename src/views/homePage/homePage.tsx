4e6bd7fe9794140059f1c0bf78b96ac35534a34d
Dynosaur <heracassia@gmail.com>
2020-02-16 23:10:20 -0600

    FC conversion, import renames

--- src/views/homePage/homePage.tsx
+++ src/views/homePage/homePage.tsx
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
+import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
-
-import Page   from '../../components/Page';
-
+import Page from '../../components/page';
 import './home-style';
 
-class HomePage extends Component {
-    render() {
-        return (
-            <Page idName='alg-home-view' navbar>
-                <h2 className='alg-home-view-title'>Algebra made easy.</h2>
-                <Link to='/graph' className='alg-home-graph-link'>
-                    <h3>Start Graphing</h3>
-                </Link>
-            </Page>
-        );
-    }
+interface HomePageProps { }
+
+const HomePage: FC<HomePageProps> = (props) => {
+    return (
+        <Page navbar pageClass='alg-home'>
+            <h2 className='alg-home-view-title'>Algebra made easy.</h2>
+            <Link to='/graph' className='alg-home-graph-link'>
+                <h3>Start Graphing</h3>
+            </Link>
+        </Page>
+    );
 }
 
 export default HomePage;
