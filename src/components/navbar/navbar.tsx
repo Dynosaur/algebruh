61655050913aa1079d4f2c493bb05a6248d7e15c
Dynosaur <heracassia@gmail.com>
2020-02-18 22:24:57 -0600

    ?

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
+import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
 import logoImage from './logo.png';
 import Switch from '../switch';
 import './navbar-style';
 
-class Navbar extends Component {
-    render() {
-        return (
-            <div className='alg-navbar'>
-                <Link to='/' className='alg-navbar-logo-link'>
-                    <img className='alg-navbar-logo-img' src={logoImage} />
+interface NavbarProps {
+    onSwitchToggle?: () => void;
+}
+
+const Navbar: FC<NavbarProps> = (props) => {
+    return (
+        <div className='alg-navbar'>
+            <Link to='/' className='alg-navbar-logo-link'>
+                <img className='alg-navbar-logo-img' src={logoImage} />
+            </Link>
+            <div className='alg-navbar-aux'>
+                <Switch onToggle={props.onSwitchToggle} />
+                <Link to='/graph' className='alg-navbar-graph-link'>
+                    <h6>Graph</h6>
                 </Link>
-                <div className='alg-navbar-aux'>
-                    <Switch />
-                    <Link to='/graph' className='alg-navbar-graph-anchor'>
-                        <h6 className='alg-navbar-text'>Graph</h6>
-                    </Link>
-                    <Link to='/sign-in'>
-                        <h6 className='alg-navbar-text'>Sign In</h6>
-                    </Link>
-                </div>
+                <Link to='/sign-in' className='alg-navbar-sign-in-link'>
+                    <h6>Sign In</h6>
+                </Link>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Navbar;
