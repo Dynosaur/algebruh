a19fa630128b1e616b221076271286145ae02371
Dynosaur <heracassia@gmail.com>
2020-02-18 22:22:15 -0600

    Typescript conversion, fetus dark mode, router is now a functional component

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
+import React, { FC, useState } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
 import GraphPage    from './views/graphPage';
 import HomePage     from './views/homePage';
 import NotFoundPage from './views/NotFoundPage';
 import SignInPage   from './views/signinPage';
-
 import './global-style';
 
-const frontEndRouter = (
-    <Router>
-        <Switch>
-            <Route exact path='/'        component={ HomePage     } />
-            <Route exact path='/home'    component={ HomePage     } />
-            <Route exact path='/sign-in' component={ SignInPage   } />
-            <Route exact path='/graph'   component={ GraphPage    } />
-            <Route                       component={ NotFoundPage } />
-        </Switch>
-    </Router>
-);
+interface MainProps {}
 
-ReactDOM.render(frontEndRouter, document.getElementById('root'));
+const Main: FC<MainProps> = () => {
+    const [darkMode, setDarkMode] = useState(false);
+    const toggleDarkMode = () => {
+        setDarkMode(!darkMode);
+    }
+
+    const home = <HomePage dark={darkMode} onNavbarSwitchToggle={toggleDarkMode} />
+
+    return(
+        <Router>
+            <Switch>
+                <Route exact path='/'>
+                    {home}
+                </Route>
+                <Route exact path='/home'>
+                    {home}
+                </Route>
+                <Route exact path='/sign-in'>
+                    <SignInPage />
+                </Route>
+                <Route exact path='/graph'>
+                    <GraphPage />
+                </Route>
+                <Route>
+                    <NotFoundPage />
+                </Route>
+            </Switch>
+        </Router>
+    );
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
