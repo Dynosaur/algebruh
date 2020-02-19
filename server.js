735c003d67fa78f94cbeafde5d4a4964b43b70ea
Dynosaur <heracassia@gmail.com>
2020-02-18 22:19:11 -0600

    Added pretty print

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const chalk = require('chalk');
+const boxen = require('boxen');
 const express = require('express');
 
 const server = express();
@@ -14,5 +16,9 @@ server.get('*', (req, res) => {
 });
 
 server.listen(port);
-console.log('Time: ' + new Date());
-console.log(`Server is listening on port ${port} ...`);
+console.log(
+    boxen(
+        chalk.gray('Time: ' + new Date()) + '\n' +
+        chalk.bold.rgb(0,225,0)(`Server is listening on port ${port} ...`)
+    , {padding: 1, borderStyle: 'round', borderColor: '#27f'})
+);
