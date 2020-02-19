d3c18309d3035a7ef60e89c94b1acfbea9d749a5
Dynosaur <heracassia@gmail.com>
2020-02-18 22:28:41 -0600

    Added home props

--- src/views/homePage/homePage.tsx
+++ src/views/homePage/homePage.tsx
@@ -3,11 +3,15 @@ import { Link } from 'react-router-dom';
 import Page from '../../components/page';
 import './home-style';
 
-interface HomePageProps { }
+interface HomePageProps {
+    dark: boolean;
+    onNavbarSwitchToggle?: () => void;
+}
 
 const HomePage: FC<HomePageProps> = (props) => {
+    const passedClass = (props.dark) ? 'alg-home dark-mode' : 'alg-home';
     return (
-        <Page navbar pageClass='alg-home'>
+        <Page navbar pageClass={passedClass} onNavbarSwitchToggle={props.onNavbarSwitchToggle}>
             <h2 className='alg-home-view-title'>Algebra made easy.</h2>
             <Link to='/graph' className='alg-home-graph-link'>
                 <h3>Start Graphing</h3>
