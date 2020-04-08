785d9c8b7e7a570a1717806527dd9766d0b493c7
Dynosaur <heracassia@gmail.com>
2020-04-07 22:18:40 -0500

    Cleaned webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,44 @@
 const path = require('path');
-const miniCSS = require('mini-css-extract-plugin');
+const miniCSSPlugin = require('mini-css-extract-plugin');
 
 const srcDir = path.resolve(__dirname, './src');
+const publicDir = path.resolve(__dirname, './dist');
 
 module.exports = {
-  mode: 'development',
-  entry: path.resolve(__dirname, './src/index.tsx'),
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(jsx?)$/,
-        include: srcDir,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.s(a|c)ss$/,
-        include: srcDir,
-        use: [miniCSS.loader, 'css-loader', 'sass-loader']
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        include: srcDir,
-        loader: 'file-loader'
-      },
-      {
-        test: /\.tsx?$/,
-        include: srcDir,
-        loader: 'ts-loader',
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.css', '.scss', '.sass', '.ts', '.tsx']
-  },
-  plugins: [new miniCSS(
-      { filename: 'index.css', chunkFilename: 'index.css' }
-  )],
-  devtool: "source-map",
-  stats: 'errors-warnings'
-};
+    mode: 'development',
+    entry: './src/index.tsx',
+    output: {
+        path: publicDir,
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                include: srcDir,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.tsx?$/,
+                include: srcDir,
+                loader: 'ts-loader'
+            },
+            {
+                test: /\.sass$/,
+                include: srcDir,
+                use: [miniCSSPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(jpg|png)$/,
+                include: srcDir,
+                loader: 'file-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.css', '.sass', '.ts', '.tsx']
+    },
+    plugins: [ new miniCSSPlugin({ filename: 'index.css', chunkFilename: 'index.css' }) ],
+    devtool: 'source-map',
+    stats: 'errors-warnings'
+}
